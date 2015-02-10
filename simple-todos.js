c40722c1147d53c7531bc4a52d0b32154793b821
Ohui A Kwao <afi.kwao@meltwater.org>
2015-02-10 15:13:02 +0000

    forms submit and order

--- simple-todos.js
+++ simple-todos.js
@@ -1,8 +1,23 @@
 Tasks = new Mongo.Collection("tasks");
 if (Meteor.isClient) {
+  Template.body.events({
+      "submit .new-task": function (event) {
+        // This function is called when the new task form is submitted
+        var text = event.target.text.value;
+        Tasks.insert({
+          text :text,
+          createdAt: new Date() // current time
+        });
+        // Clear form
+        event.target.text.value = "";
+        // Prevent default form submit
+        return false;
+      }
+    });
   Template.body.helpers({
+
     tasks: function () {
-      return Tasks.find({});
+      return Tasks.find({}, {sort: {createdAt: -1}});
     }
   });
 }
